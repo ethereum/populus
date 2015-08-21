4aa41f4ac9bd2a22622ab439efeada84a9cf0920
Piper Merriam <pipermerriam@gmail.com>
2015-08-21 10:44:49 -0600

    enable bool response values from contracts

--- tests/contracts/test_decode_single_type.py
+++ tests/contracts/test_decode_single_type.py
@@ -29,3 +29,15 @@ def test_decode_int256(input, expected):
 def test_decode_uint256(input, expected):
     output = decode_single('uint256', input)
     assert output == expected
+
+
+@pytest.mark.parametrize(
+    'input,expected',
+    (
+        ('0x0000000000000000000000000000000000000000000000000000000000000001', True),
+        ('0x0000000000000000000000000000000000000000000000000000000000000000', False),
+    )
+)
+def test_decode_bool(input, expected):
+    output = decode_single('bool', input)
+    assert output == expected

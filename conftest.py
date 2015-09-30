4a37108e206738651a4e58c438a874c81076e300
Piper Merriam <pipermerriam@gmail.com>
2015-09-30 12:27:54 -0600

    Pin versions to get tests passing

--- conftest.py
+++ conftest.py
@@ -2,14 +2,10 @@ import pytest
 
 
 @pytest.fixture(params=["ethtester", "rpc"])
-def blockchain_client(request):
+def blockchain_client(request, ethtester_client, rpc_client):
     if request.param == "ethtester":
-        from populus.ethtester_client import EthTesterClient
-        return EthTesterClient()
+        return ethtester_client
     elif request.param == 'rpc':
-        from eth_rpc_client import Client
-        rpc_host = getattr(request.module, 'rpc_server_host', '127.0.0.1')
-        rpc_port = getattr(request.module, 'rpc_server_port', 8545)
-        return Client(rpc_host, rpc_port)
+        return rpc_client
     else:
         raise ValueError("What happened!")

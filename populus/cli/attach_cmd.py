53675437e1bc37b4e622b839a92fdfb683a26255
Carl Allendorph <carl@resonantcircuitlabs.com>
2015-12-07 22:21:55 -0500

    Added 'redeploy' method that can be used to redeploy contracts. Method also updates the 'contracts' local reference with updated class objects and known instances

--- populus/cli/attach_cmd.py
+++ populus/cli/attach_cmd.py
@@ -23,9 +23,57 @@ from populus.contracts import (
 from populus.geth import (
     get_active_data_dir,
     get_known_contracts,
+    add_to_known_contracts,
+)
+from populus.deployment import (
+    deploy_contracts,
+    validate_deployed_contracts,
+)
+from populus.cli.deploy_cmd import (
+    echo_post_deploy_message
 )
 
 from .main import main
+
+
+def deploy_set(context, client, project_dir, data_dir=None, record=True, contracts_by_name=[]):
+    """ Re-Deploy a set of contracts by name or all contracts
+        @param context local context for the attach python interpreter.
+        @param client RPC client for accessing the ethereum chain
+        @param project_dir absolute file path of the project that
+            will be interrogated to determine the contracts that
+            can be deployed
+        @param data_dir directory of the test chain if not None
+        @param contracts_by_name if not None, this must be a list
+            of strings indicating the names of the contracts to deploy
+    """
+
+    if type(contracts_by_name) is not list:
+        raise TypeError("Contracts by name must be a list of strings")
+
+    contracts = package_contracts(utils.load_contracts(project_dir))
+    deployed_contracts = deploy_contracts(
+        deploy_client=client,
+        contracts=contracts,
+        deploy_at_block=1,
+        max_wait_for_deploy=120,
+        from_address=None,
+        max_wait=120,
+        contracts_to_deploy=contracts_by_name,
+        dependencies=None,
+        constructor_args=None,
+        deploy_gas=None,
+    )
+    validate_deployed_contracts(client, deployed_contracts)
+    echo_post_deploy_message(client, deployed_contracts)
+    if data_dir is not None and record:
+        add_to_known_contracts(deployed_contracts, data_dir)
+    # Update the attach shell's context with the latest contracts
+    # objects
+    context["contracts"] = contracts
+    if data_dir is not None:
+        setup_known_instances(context, data_dir)
+    return(deployed_contracts)
 
 
 def setup_known_instances(context, data_dir):
@@ -79,17 +127,27 @@ def attach(active):
     """
     project_dir = os.path.abspath(os.getcwd())
     contracts_meta = utils.load_contracts(project_dir)
+    client = Client('127.0.0.1', '8545')
 
     context = {
         'contracts': package_contracts(contracts_meta),
-        'client': Client('127.0.0.1', '8545'),
+        'client': client,
     }
+    data_dir = None
     if active:
         data_dir = get_active_data_dir(project_dir)
         if os.path.islink(data_dir):
             setup_known_instances(context, data_dir)
         else:
             click.echo(click.style("No Valid Active Chain Data Directory Found!", fg="red"))
+
+    def redeploy(contracts=[], record=True):
+        return(deploy_set(
+            context, client, project_dir, data_dir=data_dir,
+            record=record, contracts_by_name=contracts
+        ))
+
+    context["redeploy"] = redeploy
 
     contract_names = ', '.join(sorted(contracts_meta.keys()))
 
@@ -103,9 +161,15 @@ def attach(active):
 
         contracts  -> Contract classes
         client     -> Blockchain client ({client_type})
+        redeploy   -> Method to reploy project contracts
+                      Example:
+                        deployed_cts = redeploy()
+                        deployed_cts = redeploy(record = False)
+                        deployed_cts = redeploy(contracts = ["Example"])
 
         Contracts: {contracts}
         Check contracts.<type>.known for deployed contracts.
+
         """
     ).format(
         python_version=sys.version.partition('\n')[0],

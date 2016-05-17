356fa45de6d21119bd5f2ec27baa9c73c341ab66
Martin Holst Swende <martin@swende.se>
2016-05-17 10:41:03 +0200

    Remove unused code

--- populus/geth.py
+++ populus/geth.py
@@ -181,13 +181,6 @@ def geth_wrapper(data_dir, popen_class=subprocess.Popen, cmd="geth",
     if miner_threads is not None:
         command.extend(('--minerthreads', miner_threads))
 
-    # This parameter is deprecated, 
-    #see https://github.com/pipermerriam/populus/issues/79
-    #if logfile is not None:
-    #    command.extend((
-    #        '--logfile', logfile,
-    #    ))
-
     if verbosity is not None:
         command.extend((
             '--verbosity', verbosity,

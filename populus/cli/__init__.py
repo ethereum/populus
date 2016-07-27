de7be7fc2f06651e0cb471fdf88b36389f555ac2
Piper Merriam <pipermerriam@gmail.com>
2016-07-27 16:44:09 -0600

    remove attach

--- populus/cli/__init__.py
+++ populus/cli/__init__.py
@@ -1,5 +1,4 @@
 from .main import main  # NOQA
-from .attach_cmd import attach  # NOQA
 from .chain_cmd import chain  # NOQA
 from .compile_cmd import compile_contracts  # NOQA
 from .deploy_cmd import deploy  # NOQA

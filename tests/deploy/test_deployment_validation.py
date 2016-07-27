388f6b970de033e42325cb807dad962bcd2c2160
Piper Merriam <pipermerriam@gmail.com>
2016-07-27 13:00:53 -0600

    max_wait to timeout keyword arg rename

--- tests/deploy/test_deployment_validation.py
+++ tests/deploy/test_deployment_validation.py
@@ -8,7 +8,7 @@ from populus.deployment import (
 
 def test_validation_fails_if_no_code(web3, MATH):
     contracts = {'Math': MATH}
-    deployed_contracts = deploy_contracts(web3, contracts, max_wait=1, txn_defaults={'gas': 1})
+    deployed_contracts = deploy_contracts(web3, contracts, txn_defaults={'gas': 1}, timeout=30)
 
     deployed_contracts.Math.address = "0xd3cda913deb6f67967b99d67acdfa1712c293601"
 

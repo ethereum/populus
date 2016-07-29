48ab678f12a459dda1f03d675fdf7e339560f0d6
euri10 <benoit.barthelet@gmail.com>
2016-07-29 10:36:42 +0200

    init working

--- populus/cli/chain_cmd.py
+++ populus/cli/chain_cmd.py
@@ -52,62 +52,12 @@ def chain_reset(name, confirm):
     help="""
     Set verbosity of the logging output. Default is 5, Range is 0-6.
     """)
-<<<<<<< HEAD
-@click.option(
-    '--active/--no-active', default=True,
-    help="""
-    Set whether the chain run will modify the active-chain settings.
-    Default is to modify the active-chain setting.
-    """)
-@click.option(
-     '--rpccorsdomain', default=None,
-     help="""
-     Determines the value that will be passed in as the `--rpcorsdomain` to the `geth` instance.
-     """)
-def chain_run(name, mine, verbosity, active, rpccorsdomain):
-=======
 def chain_run(name, mine, verbosity):
->>>>>>> upstream/master
     """
     Run a geth node.
     """
     project_dir = os.getcwd()
-<<<<<<< HEAD
-    data_dir = get_geth_data_dir(project_dir, name)
-    logfile_path = get_geth_logfile_path(data_dir)
-
-    ensure_account_exists(data_dir)
-
-    kwargs = {
-#        "logfile": logfile_path,
-        "verbosity": "%d" % verbosity,
-        "rpccorsdomain": 'http://localhost:5000'
-        }
-
-    command, proc = run_geth_node(data_dir, mine=mine,  **kwargs)
-
-    click.echo("Running: '{0}'".format(' '.join(command)))
-
-    if active:
-        set_active_data_dir(project_dir, name)
-
-    try:
-        while True:
-            out_line = proc.get_stdout_nowait()
-            if out_line:
-                click.echo(out_line, nl=False)
-
-            err_line = proc.get_stderr_nowait()
-            if err_line:
-                click.echo(err_line, nl=False)
-
-            if err_line is None and out_line is None:
-                time.sleep(0.2)
-    except KeyboardInterrupt:
-=======
-
     with dev_geth_process(project_dir, name):
->>>>>>> upstream/master
         try:
             while True:
                 gevent.sleep(0)

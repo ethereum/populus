e4a8a23200c0e11c1adb23a098121d1e93142b6b
Piper Merriam <pipermerriam@gmail.com>
2016-08-11 10:35:59 -0600

    remove monkeypatch alltogether

--- populus/cli/__init__.py
+++ populus/cli/__init__.py
@@ -1,11 +1,3 @@
-# Patch stdlib when using Populus.
-# This should happen only when we are using populus as
-# standanlone CLI application.
-# https://github.com/pipermerriam/populus/issues/117
-from gevent import monkey
-monkey.patch_all()
-
-
 from .main import main  # NOQA
 from .chain_cmd import chain  # NOQA
 from .compile_cmd import compile_contracts  # NOQA

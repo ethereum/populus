5851b3f25373b8d6ea2d2b99f98932ec8d0ec820
Piper Merriam <pipermerriam@gmail.com>
2016-08-12 23:48:19 -0600

    partially update the deploy command

--- populus/cli/deploy_cmd.py
+++ populus/cli/deploy_cmd.py
@@ -1,25 +1,10 @@
-import os
-
 import click
 
-from geth import (
-    DevGethProcess,
+from populus.utils.cli import (
+    select_chain,
+    show_chain_sync_progress,
 )
 
-from web3 import (
-    Web3,
-    IPCProvider,
-)
-
-from populus.utils.contracts import (
-    load_compiled_contract_json,
-)
-from populus.utils.transactions import (
-    wait_for_block_number,
-)
-from populus.compilation import (
-    compile_and_write_contracts,
-)
 from populus.deployment import (
     deploy_contracts,
     validate_deployed_contracts,
@@ -57,53 +42,35 @@ def echo_post_deploy_message(web3, deployed_contracts):
 )
 # Deploy chain config
 @click.option(
-    '--chain',
+    '--chain-name',
     '-c',
     default='default',
     help="Specify which chain to deploy to.",
 )
-# Compilation config
-@click.option(
-    '--compile/--no-compile',
-    default=True,
-    help="Should contracts be compiled",
-)
-@click.option(
-    '--optimize/--no-optimize',
-    default=True,
-    help="Should contracts be compiled with the --optimize flag.",
-)
 @click.argument('contracts_to_deploy', nargs=-1)
-def deploy(chain, confirm, compile, optimize, contracts_to_deploy):
+@click.pass_context
+def deploy(ctx, chain_name, contracts_to_deploy):
     """
     Deploys the specified contracts via the RPC client.
     """
-    # TODO: project_dir should happen up at the `main` level
-    project_dir = os.getcwd()
+    project = ctx.obj['PROJECT']
 
-    if compile:
-        compile_and_write_contracts(project_dir, optimize=optimize)
+    # Determine which chain should be used.
+    if not chain_name:
+        try:
+            chain_name = ctx.obj['CHAIN_NAME']
+        except KeyError:
+            chain_name = select_chain(project)
 
-    compiled_contracts = load_compiled_contract_json(project_dir)
+    compiled_contracts = project.compiled_contracts
 
-    # TODO: what if the user wants to run geth themselves.
-    with DevGethProcess(chain_name=chain, base_dir=project_dir) as geth:
-        geth.wait_for_dag(600)
-        geth.wait_for_ipc(30)
+    chain = project.get_chain(chain_name)
 
-        web3 = Web3(IPCProvider(geth.ipc_path))
+    with chain:
+        web3 = chain.web3
 
-        wait_for_block_number(web3, 1, 120)
-
-        if confirm:
-            message = (
-                "You are about to deploy contracts to a production environment. "
-                "You must have an RPC server that is unlocked running for this to "
-                "work.\n\n"
-                "Would you like to proceed?"
-            )
-            if not click.confirm(message):
-                raise click.Abort()
+        if chain_name in {'mainnet', 'morden'}:
+            show_chain_sync_progress(chain)
 
         deployed_contracts = deploy_contracts(
             web3,

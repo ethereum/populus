0a202c538648de0e895bb041c8a302e64beef56a
Piper Merriam <pipermerriam@gmail.com>
2016-08-18 13:54:22 -0600

    Chain config

--- populus/cli/__init__.py
+++ populus/cli/__init__.py
@@ -3,6 +3,10 @@ from .chain_cmd import chain_cmd  # NOQA
 from .compile_cmd import compile_contracts  # NOQA
 from .deploy_cmd import deploy  # NOQA
 from .init_cmd import init  # NOQA
-from .config_cmd import config  # NOQA
+from .config_cmd import (  # NOQA
+    config,
+    config_set,
+    config_unset,
+)
 from .makemigration_cmd import make_migration  # NOQA
 from .migrate_cmd import migrate  # NOQA

d8bde5ce35b9d6c1ee8a108fd2cdb1732cc5c91e
Piper Merriam <pipermerriam@gmail.com>
2016-08-23 21:04:49 -0600

    v1.0.0b7 Release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ readme = open(os.path.join(DIR, 'README.md')).read()
 
 setup(
     name='populus',
-    version="1.0.0b6",
+    version="1.0.0b7",
     description="""Ethereum Development Framework""",
     long_description=readme,
     author='Piper Merriam',

b75821bf5afff4fe89c3089da5dcddaa854e9d7f
Piper Merriam <pipermerriam@gmail.com>
2016-08-31 17:03:26 -0600

    update path tests to use is_same_path

--- tests/project/test_project_object_defaults_to_cwd.py
+++ tests/project/test_project_object_defaults_to_cwd.py
@@ -1,7 +1,8 @@
+from populus.utils.filesystem import is_same_path
 from populus.project import Project
 
 
 def test_project_dir_defaults_to_cwd(project_dir):
     project = Project()
 
-    assert project.project_dir == project_dir
+    assert is_same_path(project.project_dir, project_dir)

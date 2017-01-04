34c8fdf9431e7cf4b214e50d8e06ca7e35562624
Piper Merriam <pipermerriam@gmail.com>
2017-01-04 12:14:02 -0700

    fix external chain

--- populus/chain.py
+++ populus/chain.py
@@ -450,9 +450,6 @@ class ExternalChain(Chain):
     Chain class to represent an externally running blockchain that is not
     locally managed.  This class only houses a pre-configured web3 instance.
     """
-    def __enter__(self):
-        return self
-
     @property
     def has_registrar(self):
         return 'registrar' in self.config

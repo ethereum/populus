1573c59a317bda3284f70b48c61f8f4be75cafd1
Piper Merriam <pipermerriam@gmail.com>
2017-01-06 07:39:15 -0700

    dirty

--- populus/packages/backends/ipfs.py
+++ populus/packages/backends/ipfs.py
@@ -39,6 +39,12 @@ class BaseIPFSPackageBackend(BasePackageBackend):
             return True
         return False
 
+    def resolve_to_release_lockfile(self, package_identifier):
+        ipfs_path = extract_ipfs_path_from_uri(package_identifier)
+        lockfile_contents = self.get_file_from_ipfs(ipfs_path)
+        release_lockfile = json.loads(force_text(lockfile_contents))
+        return release_lockfile
+
     def can_resolve_package_source_tree(self, release_lockfile):
         sources = release_lockfile.get('sources')
         if sources is None:
@@ -50,6 +56,23 @@ class BaseIPFSPackageBackend(BasePackageBackend):
     def can_persist_package_file(self, file_path):
         return True
 
+    def persist_package_file(self, file_path):
+        """
+        Persists the provided file to this backends persistence layer.
+        """
+        ipfs_file_hash = self.push_file_to_ipfs(file_path)
+        ipfs_uri = create_ipfs_uri(ipfs_file_hash)
+        return ipfs_uri
+
+    #
+    # Subclass API
+    #
+    def push_file_to_ipfs(self, file_path):
+        raise NotImplementedError("Must be implemented by subclasses")
+
+    def get_file_from_ipfs(self, ipfs_path):
+        raise NotImplementedError("Must be implemented by subclasses")
+
 
 class IPFSPackageBackend(BaseIPFSPackageBackend):
     """
@@ -59,12 +82,6 @@ class IPFSPackageBackend(BaseIPFSPackageBackend):
         ipfs_host = self.settings['host']
         ipfs_port = self.settings['port']
         self.ipfs_client = ipfsapi.connect(ipfs_host, ipfs_port)
-
-    def resolve_to_release_lockfile(self, package_identifier):
-        ipfs_path = extract_ipfs_path_from_uri(package_identifier)
-        lockfile_contents = self.ipfs_client.cat(ipfs_path)
-        release_lockfile = json.loads(force_text(lockfile_contents))
-        return release_lockfile
 
     @cast_return_to_dict
     def resolve_package_source_tree(self, release_lockfile):
@@ -80,11 +97,14 @@ class IPFSPackageBackend(BaseIPFSPackageBackend):
             else:
                 yield source_path, source_value
 
-    def persist_package_file(self, file_path):
+    def push_file_to_ipfs(self, file_path):
         """
         Persists the provided file to this backends persistence layer.
         """
         result = self.ipfs_client.add(file_path)
         ipfs_file_hash = result['Hash']
-        ipfs_uri = create_ipfs_uri(ipfs_file_hash)
-        return ipfs_uri
+        return ipfs_file_hash
+
+    def get_file_from_ipfs(self, ipfs_path):
+        file_contents = self.ipfs_client.cat(ipfs_path)
+        return file_contents

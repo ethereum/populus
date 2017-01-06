157708c418a1a9bf0b8f4fc616cb4063ba282ac5
Piper Merriam <pipermerriam@gmail.com>
2017-01-05 17:16:10 -0700

    more tests

--- populus/packages/backends/lockfile.py
+++ populus/packages/backends/lockfile.py
@@ -14,17 +14,24 @@ class LocalFilesystemLockfileBackend(BasePackageBackend):
     """
     Backend for package installation that can be used to install the current package.
     """
+    def can_translate_package_identifier(self, package_identifier):
+        return is_aliased_filesystem_release_lockfile_path(package_identifier)
+
+    def translate_package_identifier(self, package_identifier):
+        if is_aliased_filesystem_release_lockfile_path(package_identifier):
+            _, _, release_lockfile_path = package_identifier.partition('@')
+            return (
+                release_lockfile_path,
+            )
+        else:
+            raise ValueError("Unsupported identifier: {0}".format(package_identifier))
+
     def can_resolve_to_release_lockfile(self, package_identifier):
-        return any((
-            is_filesystem_release_lockfile_path(package_identifier),
-            is_aliased_filesystem_release_lockfile_path(package_identifier),
-        ))
+        return is_filesystem_release_lockfile_path(package_identifier)
 
     def resolve_to_release_lockfile(self, package_identifier):
         if is_filesystem_release_lockfile_path(package_identifier):
             release_lockfile_path = package_identifier
-        elif is_aliased_filesystem_release_lockfile_path(package_identifier):
-            _, _, release_lockfile_path = package_identifier.partition('@')
         else:
             raise ValueError("Unsupported identifier: {0}".format(package_identifier))
 

3673ec4324547adb8316e6fcafb31df80e9df41b
Piper Merriam <pipermerriam@gmail.com>
2017-01-06 11:45:56 -0700

    dirty

---
+++ tests/packaging-utils/test_is_dependency_version.py
@@ -0,0 +1,25 @@
+import pytest
+
+from populus.utils.packaging import (
+    is_dependency_version,
+)
+
+
+@pytest.mark.parametrize(
+    "value,expected",
+    (
+        # Good
+        ('1.0.0', True),
+        ('1.0.0-beta1', True),
+        ('1.0.0-beta1+d3af3487', True),
+        ('>1.0.0', True),
+        ('>=1.0.0', True),
+        ('<1.0.0', True),
+        ('<=1.0.0', True),
+        # Bad
+        ('==1.0.0', False),
+    )
+)
+def test_is_dependency_version(value, expected):
+    actual = is_dependency_version(value)
+    assert actual is expected

0584e2e863cca454d49c110930864bad5291b61f
Piper Merriam <pipermerriam@gmail.com>
2017-01-13 14:02:33 -0700

    patch all

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import os
 
 if os.environ.get('THREADING_BACKEND', 'stdlib') == 'gevent':
     from gevent import monkey
-    monkey.patch_socket()
+    monkey.patch_all()
 
 import pytest  # noqa: E402
 

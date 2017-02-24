598f395ae8781550905f683c3835e6c836b1140f
Piper Merriam <pipermerriam@gmail.com>
2017-02-24 18:15:51 -0500

    pull in backend initialization

--- populus/chain/base.py
+++ populus/chain/base.py
@@ -4,11 +4,31 @@ import warnings
 
 from pylru import lrucache
 
+from eth_utils import (
+    to_ordered_dict,
+)
+
+from populus.contracts.provider import (
+    Provider,
+)
+from populus.contracts.registrar import (
+    Registrar,
+)
+from populus.contracts.store import (
+    Store,
+)
+
+from populus.utils.config import (
+    sort_prioritized_configs,
+)
 from populus.utils.contracts import (
     construct_contract_factories,
 )
 from populus.utils.functional import (
     cached_property,
+)
+from populus.utils.module_loading import (
+    import_string,
 )
 from populus.utils.wait import (
     Wait,
@@ -59,6 +79,83 @@ class BaseChain(object):
     @property
     def wait(self):
         return Wait(self.web3)
+
+    #
+    # +--------------+
+    # | Contract API |
+    # +--------------+
+    #
+    @property
+    def contract_backend_configs(self):
+        config = self.config.get_config('contracts.backends')
+        return sort_prioritized_configs(config, self.project.config)
+
+    @cached_property
+    @to_ordered_dict
+    def contract_backends(self):
+        for backend_name, backend_config in self.contract_backend_configs.items():
+            ProviderBackendClass = import_string(backend_config['class'])
+            yield (
+                backend_name,
+                ProviderBackendClass(self, backend_config.get_config('settings')),
+            )
+
+    #
+    # Provider
+    #
+    @property
+    @to_ordered_dict
+    def provider_backends(self):
+        for backend_name, backend in self.contract_backends.items():
+            if backend.is_provider:
+                yield backend_name, backend
+
+    @property
+    def provider(self):
+        if not self.provider_backends:
+            raise ValueError(
+                "Must have at least one provider backend "
+                "configured\n{0}".format(self.backend_configs)
+            )
+        return Provider(self, self.provider_backends)
+
+    #
+    # Registrar
+    #
+    @cached_property
+    @to_ordered_dict
+    def registrar_backends(self):
+        for backend_name, backend in self.contract_backends.items():
+            if backend.is_registrar:
+                yield backend_name, backend
+
+    @property
+    def registrar(self):
+        if not self.registrar_backends:
+            raise ValueError(
+                "Must have at least one registrar backend "
+                "configured\n{0}".format(self.backend_configs)
+            )
+        return Registrar(self, self.registrar_backends)
+
+    #
+    # Source
+    #
+    @property
+    @to_ordered_dict
+    def store_backends(self):
+        for backend_name, backend in self.contract_backends.items():
+            if backend.is_store:
+                yield backend_name, backend
+
+    @property
+    def store(self):
+        if not self.store_backends:
+            raise ValueError(
+                "Must have at least one store backend "
+                "configured\n{0}".format(self.backend_configs)
+            )
+        return Store(self, self.store_backends)
 
     #
     # Running the chain

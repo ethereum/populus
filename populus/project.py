fb8ad175af637ba06c3253badeddf197edf61622
aviah <aviah@gmail.com>
2017-08-05 11:50:22 -0700

    piper populus/project.py from feat/v2 merged

--- populus/project.py
+++ populus/project.py
@@ -1,6 +1,11 @@
 import itertools
-import os
 import sys
+import json
+import os
+
+from eth_utils import (
+    to_dict,
+)
 
 from populus.compilation import (
     compile_project_contracts,
@@ -21,14 +26,13 @@ from populus.utils.compile import (
     get_build_asset_dir,
     get_compiled_contracts_asset_path,
     get_contracts_source_dir,
+    get_dependency_source_paths,
     get_project_source_paths,
     get_test_source_paths,
 )
 
 from populus.utils.filesystem import (
     relpath,
-    get_latest_mtime,
-    is_same_path,
 )
 from populus.utils.config import (
     check_if_json_config_file_exists,
@@ -59,7 +63,7 @@ class Project(object):
         if not self.has_json_config():
             raise FileNotFoundError(
                 "Did not find config file {file_name} in {dir_name}".format(
-                    file_name=JSON_PROJECT_CONFIG_FILENAME,dir_name=self.project_root_dir)
+                    file_name=JSON_PROJECT_CONFIG_FILENAME, dir_name=self.project_root_dir)
             )
         self.json_config_file_path = os.path.join(self.project_root_dir, JSON_PROJECT_CONFIG_FILENAME)
         self.load_config()
@@ -67,7 +71,6 @@ class Project(object):
         if not any(is_same_path(p, project_root_dir) for p in sys.path):
             # ensure that the project directory is in the sys.path
             sys.path.insert(0, project_root_dir)
-
 
     def create(self):
 
@@ -141,7 +144,51 @@ class Project(object):
         return get_tests_dir(self.project_dir)
 
     #
-    # Contracts
+    # Packaging: Installed Packages
+    #
+    @property
+    def dependencies(self):
+        if self.has_package_manifest:
+            package_manifest = self.package_manifest
+        else:
+            package_manifest = {}
+        package_dependencies = package_manifest.get('dependencies', {})
+        return package_dependencies
+
+    @property
+    @relpath
+    def installed_packages_dir(self):
+        return get_installed_packages_dir(self.project_dir)
+
+    @property
+    @to_dict
+    def installed_dependency_locations(self):
+        # TODO: rename to `installed_dependency_locations`
+        return get_installed_dependency_locations(self.installed_packages_dir)
+
+    #
+    # Packaging: Backends
+    #
+    @to_dict
+    def get_package_backend_config(self):
+        package_backend_config = self.config.get_config('packaging.backends')
+        return sort_prioritized_configs(package_backend_config, self.config)
+
+    @cached_property
+    @to_dict
+    def package_backends(self):
+        for backend_name, backend_config in self.get_package_backend_config().items():
+            PackageBackendClass = import_string(backend_config['class'])
+            yield (
+                backend_name,
+                PackageBackendClass(
+                    self,
+                    backend_config.get_config('settings'),
+                ),
+            )
+
+    #
+    # Contract Source and Compilation
     #
     @property
     @relpath
@@ -170,6 +217,23 @@ class Project(object):
             get_test_source_paths(self.tests_dir),
         ))
 
+    def get_source_modification_time(self):
+        dependency_source_paths = tuple(itertools.chain.from_iterable(
+                get_dependency_source_paths(dependency_base_dir)
+                for dependency_base_dir
+                in recursive_find_installed_dependency_base_dirs(self.installed_packages_dir)
+            ))
+        all_source_paths = tuple(itertools.chain(
+                get_project_source_paths(self.contracts_source_dir),
+                get_test_source_paths(self.tests_dir),
+                dependency_source_paths,
+            ))
+        return max(
+                os.path.getmtime(source_file_path)
+                for source_file_path
+                in all_source_paths
+                ) if len(all_source_paths) > 0 else None
+
     def is_compiled_contract_cache_stale(self):
         if self._cached_compiled_contracts is None:
             return True
@@ -183,24 +247,29 @@ class Project(object):
         else:
             return self._cached_compiled_contracts_mtime < source_mtime
 
-    def fill_contracts_cache(self, compiled_contracts, contracts_mtime):
+    def fill_contracts_cache(self, contracts, contracts_mtime):
         """
         :param contracts: become the Project's cache for compiled contracts
         :param contracts_mtime: last modification of supplied contracts
         :return:
         """
         self._cached_compiled_contracts_mtime = contracts_mtime
-        self._cached_compiled_contracts = compiled_contracts
+        # self._cached_compiled_contracts = compiled_contracts
+        self._cached_compiled_contracts = contracts
 
     @property
     def compiled_contract_data(self):
         if self.is_compiled_contract_cache_stale():
+            """ was
             source_file_paths, compiled_contracts = compile_project_contracts(self)
             contracts_mtime = get_latest_mtime(source_file_paths)
             self.fill_contracts_cache(
                 compiled_contracts=compiled_contracts,
                 contracts_mtime=contracts_mtime,
-            )
+            )"""
+            self._cached_compiled_contracts_mtime = self.get_source_modification_time()
+            _, self._cached_compiled_contracts = compile_project_contracts(self)
+
         return self._cached_compiled_contracts
 
     #

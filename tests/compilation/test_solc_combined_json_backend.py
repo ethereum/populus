66d919a482ba8ff5046a068f1c41d0e784eac370
Piper Merriam <pipermerriam@gmail.com>
2017-08-08 10:00:46 -0600

    remove verbose skipping

--- tests/compilation/test_solc_combined_json_backend.py
+++ tests/compilation/test_solc_combined_json_backend.py
@@ -34,10 +34,12 @@ _populus_config_key_value_pairs = (
 GREETER_SOURCE_PATH = os.path.join(ASSETS_DIR, 'Greeter.sol')
 
 
-@pytest.mark.skipif(
+pytestmark = pytest.mark.skipif(
     not get_solc_version() in Spec('<=0.4.8'),
     reason="Solc compiler not supported for combined json compilation",
 )
+
+
 @load_contract_fixture('Math.sol')
 def test_compiling_project_contracts(project):
     source_paths, compiled_contracts = compile_project_contracts(project)
@@ -51,10 +53,6 @@ def test_compiling_project_contracts(project):
     assert 'abi' in contract_data
 
 
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 @load_contract_fixture('ImportTestA.sol')
 @load_contract_fixture('ImportTestB.sol')
 @load_contract_fixture('ImportTestC.sol')
@@ -64,12 +62,6 @@ def test_compiling_with_local_project_imports(project):
     assert 'ImportTestA' in compiled_contracts
     assert 'ImportTestB' in compiled_contracts
     assert 'ImportTestC' in compiled_contracts
-
-
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 
 
 @load_contract_fixture('RemapImported.sol')
@@ -87,10 +79,6 @@ def test_compiling_with_import_remappings(project):
     assert 'RemapImportedNotUsed' in compiled_contracts
 
 
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 @load_test_contract_fixture('TestMath.sol')
 def test_compiling_with_test_contracts(project):
     source_paths, compiled_contracts = compile_project_contracts(project)
@@ -98,10 +86,6 @@ def test_compiling_with_test_contracts(project):
     assert 'TestMath' in compiled_contracts
 
 
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 @load_contract_fixture('Abstract.sol')
 def test_compiling_with_abstract_contract(project):
     _, compiled_contracts = compile_project_contracts(project)
@@ -109,10 +93,6 @@ def test_compiling_with_abstract_contract(project):
     assert 'Abstract' in compiled_contracts
 
 
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 @load_contract_fixture('Abstract.sol')
 @load_contract_fixture('UsesAbstract.sol')
 def test_compiling_with_abstract_contract_inhereted(project):
@@ -122,10 +102,6 @@ def test_compiling_with_abstract_contract_inhereted(project):
     assert 'UsesAbstract' in compiled_contracts
 
 
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 @load_contract_fixture(GREETER_SOURCE_PATH)
 def test_compiling_example_greeter_contract(project):
     _, compiled_contracts = compile_project_contracts(project)
@@ -133,10 +109,6 @@ def test_compiling_example_greeter_contract(project):
     assert 'Greeter' in compiled_contracts
 
 
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 @load_contract_fixture('Library13.sol')
 @load_contract_fixture('Multiply13.sol')
 def test_link_reference_extraction_from_bytecode(project):
@@ -148,10 +120,6 @@ def test_link_reference_extraction_from_bytecode(project):
     assert 'Library13' in compiled_contracts['Multiply13']['direct_dependencies']
 
 
-@pytest.mark.skipif(
-    not get_solc_version() in Spec('<=0.4.8'),
-    reason="Solc compiler not supported for combined json compilation",
-)
 @load_contract_fixture('Library13.sol', 'contracts/long-path-to-truncate-linkref-placeholders/Library13.sol')
 @load_contract_fixture('Multiply13.sol', 'contracts/long-path-to-truncate-linkref-placeholders/Multiply13.sol')
 def test_detects_contract_name_truncation_from_long_file_paths(project):

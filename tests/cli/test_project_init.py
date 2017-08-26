148f468249b6e898ddefa4f3eb341e64ff26fa6e
aviah <aviah@gmail.com>
2017-08-26 09:50:36 -0700

    pytest waypoint project init

--- tests/cli/test_project_init.py
+++ tests/cli/test_project_init.py
@@ -1,25 +1,27 @@
 import os
+import shutil
 import pytest
 from click.testing import CliRunner
 
 from populus.cli import main
+from populus.defaults import PROJECT_JSON_CONFIG_FILENAME
 
+def test_initializing_empty_project(project):
 
-def test_initializing_empty_project(project_dir):
-    contracts_source_dir = get_contracts_source_dir(project_dir)
-
-    os.rmdir(contracts_source_dir)
+    os.remove(
+        os.path.join(project.project_root_dir, PROJECT_JSON_CONFIG_FILENAME)
+    )
+    shutil.rmtree(project.contracts_source_dir)
+    shutil.rmtree(project.tests_dir)
+    shutil.rmtree(project.build_asset_dir)
 
     expected_paths = (
-        os.path.join(project_dir, 'populus.json'),
-        contracts_source_dir,
-        os.path.join(contracts_source_dir, 'Greeter.sol'),
-        os.path.join(project_dir, 'tests'),
-        os.path.join(project_dir, 'tests', 'test_greeter.py'),
-    )
+        os.path.join(project.project_root_dir, PROJECT_JSON_CONFIG_FILENAME),
+        os.path.join(project.contracts_source_dir, 'Greeter.sol'),
+        os.path.join(project.tests_dir, 'test_greeter.py'),
+        project.build_asset_dir
 
-    for path in expected_paths:
-        assert not os.path.exists(path)
+    )
 
     runner = CliRunner()
     result = runner.invoke(main, ['init'])

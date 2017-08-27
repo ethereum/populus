8f2f97b34f7dc5e1cc9b24c3db258656264341fa
aviah <aviah@gmail.com>
2017-08-26 22:57:36 -0700

    pytest waypoint filesystem utils

--- tests/filesystem-utils/test_is_same_path_util.py
+++ tests/filesystem-utils/test_is_same_path_util.py
@@ -3,21 +3,21 @@ import os
 from populus.utils.filesystem import is_same_path
 
 
-def test_paths_that_dont_exist(project_dir):
+def test_paths_that_dont_exist():
     assert not os.path.exists('not-a-real-path')
     assert not os.path.exists('also-not-a-real-path')
 
     assert not is_same_path('not-a-real-path', 'also-not-a-real-path')
 
 
-def test_paths_that_exist(project_dir, write_project_file):
+def test_paths_that_exist(write_project_file):
     write_project_file('not-a-real-path')
     write_project_file('also-not-a-real-path')
 
     assert not is_same_path('not-a-real-path', 'also-not-a-real-path')
 
 
-def test_relpath_and_abspath(project_dir):
+def test_relpath_and_abspath():
     abs_path = os.path.abspath('this-is-a-path')
     rel_path = os.path.relpath('this-is-a-path')
 

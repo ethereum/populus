e17eec819477b2d3bf290ea2c6e4a4977b26c491
aviah <aviah@gmail.com>
2017-08-26 23:29:53 -0700

    CHANGED TESTS pytest waypoint project deleted directories tests since they are now same as project and not provided by outer funcs

--- tests/project/test_get_chain.py
+++ tests/project/test_get_chain.py
@@ -12,16 +12,18 @@ from populus.utils.networking import (
     get_open_port,
 )
 
+from populus.chain.helpers import (
+    get_chain,
+)
 
 TESTNET_BLOCK_1_HASH = '0xad47413137a753b2061ad9b484bf7b0fc061f654b951b562218e9f66505be6ce'
 MAINNET_BLOCK_1_HASH = '0x88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6'
 
 
 @pytest.mark.slow
-def test_project_tester_chain(project_dir):
-    project = Project()
+def test_project_tester_chain(project, user_config):
 
-    chain = project.get_chain('tester')
+    chain = get_chain('tester', user_config, chain_dir=project.project_root_dir)
 
     with chain as running_tester_chain:
         web3 = running_tester_chain.web3
@@ -29,10 +31,9 @@ def test_project_tester_chain(project_dir):
 
 
 @pytest.mark.slow
-def test_project_testrpc_chain(project_dir):
-    project = Project()
+def test_project_testrpc_chain(project, user_config):
 
-    chain = project.get_chain('testrpc')
+    chain = get_chain('testrpc', user_config, chain_dir=project.project_root_dir)
 
     with chain as running_tester_chain:
         web3 = running_tester_chain.web3
--- tests/project/test_project_object_defaults_to_cwd.py
+++
@@ -1,8 +0,0 @@
-from populus.utils.filesystem import is_same_path
-from populus.project import Project
-
-
-def test_project_dir_defaults_to_cwd(project_dir):
-    project = Project()
-
-    assert is_same_path(project.project_dir, project_dir)

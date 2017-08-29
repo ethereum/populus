8df67a76900fc5cb217981444519a770233b268d
aviah <aviah@gmail.com>
2017-08-29 07:57:34 -0700

    tests configurations

--- tests/configuration/test_default_config_is_valid.py
+++ tests/configuration/test_default_config_is_valid.py
@@ -1,7 +1,7 @@
 from populus import Project
 
 
-def test_default_configuration_is_valid(project_dir):
-    project = Project()
+def test_default_configuration_is_valid(project):
+
     assert project._project_config_schema is not None
     project.config.validate()

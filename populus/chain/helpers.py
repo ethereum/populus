03a6ae8e064699f43d113f0fcce7e4da9797c9bb
aviah <aviah@gmail.com>
2017-08-29 05:56:16 -0700

    cleanup mv general utils to context: chains

--- populus/chain/helpers.py
+++ populus/chain/helpers.py
@@ -7,32 +7,55 @@ from eth_utils import (
     is_integer,
 )
 
-from .filesystem import (
+from populus.utils.filesystem import (
     normpath,
 )
-from .six import (
+
+from populus.utils.six import (
     parse,
 )
 
-
-BASE_BLOCKCHAIN_STORAGE_DIR = "./chains"
-
-
-@normpath
-def get_base_blockchain_storage_dir(project_dir):
-    base_blochcain_storage_dir = os.path.join(project_dir, BASE_BLOCKCHAIN_STORAGE_DIR)
-    return base_blochcain_storage_dir
+from populus.config.chain import (
+    ChainConfig,
+)
 
 
+def get_chain_config(chain_name, user_config):
+
+    chain_config_key = 'chains.{chain_name}'.format(chain_name=chain_name)
+
+    if chain_config_key in user_config:
+        return user_config.get_config(chain_config_key, config_class=ChainConfig)
+    else:
+        raise KeyError(
+                "Unknown chain: {0!r} - Must be one of {1!r}".format(
+                    chain_name,
+                    sorted(user_config.get('chains', {}).keys()),
+                )
+            )
+
+
+def get_chain(chain_name, user_config, chain_dir=None):
+    """
+    Returns a context manager that runs a chain within the context of the
+    current populus project.
+
+    Alternatively you can specify any chain name that is present in the
+    `chains` configuration key.
+    """
+
+    chain_config = get_chain_config(chain_name, user_config)
+    chain = chain_config.chain_class(chain_name, chain_config, user_config, chain_dir)
+    return chain
+
+
+BLOCK = 'block'
+TRANSACTION = 'transaction'
 BLOCK_OR_TRANSACTION_HASH_REGEX = "^(?:0x)?[a-zA-Z0-9]{64}$"
 
 
 def is_block_or_transaction_hash(value):
     return bool(re.match(BLOCK_OR_TRANSACTION_HASH_REGEX, value))
-
-
-BLOCK = 'block'
-TRANSACTION = 'transaction'
 
 
 def create_BIP122_uri(chain_id, resource_type, resource_identifier):

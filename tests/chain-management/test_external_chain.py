07d35699855d8904feeefd0ba3cbf5c0e43ca140
aviah <aviah@gmail.com>
2017-08-29 07:37:55 -0700

    tests chain-management

--- tests/chain-management/test_external_chain.py
+++ tests/chain-management/test_external_chain.py
@@ -2,12 +2,20 @@ import pytest
 
 from web3.providers.tester import EthereumTesterProvider
 
+from populus.chain.helpers import (
+    get_chain,
+)
 
-def test_external_chain(project):
-    project.config['chains.external.chain.class'] = 'populus.chain.ExternalChain'
-    project.config['chains.external.web3.provider.class'] = 'web3.providers.tester.EthereumTesterProvider'
-    project.config['chains.external.contracts.backends.Memory'] = {'$ref': 'contracts.backends.Memory'}
-    project.write_config()
+from populus.api.config import (
+    write_user_config,
+)
 
-    with project.get_chain('external') as external_chain:
+def test_external_chain(project, user_config, user_config_path):
+
+    user_config['chains.external.chain.class'] = 'populus.chain.ExternalChain'
+    user_config['chains.external.web3.provider.class'] = 'web3.providers.tester.EthereumTesterProvider'
+    user_config['chains.external.contracts.backends.Memory'] = {'$ref': 'contracts.backends.Memory'}
+    write_user_config(user_config, user_config_path)
+
+    with get_chain("external", user_config, chain_dir=project.project_root_dir) as external_chain:
         assert isinstance(external_chain.web3.currentProvider, EthereumTesterProvider)
--- tests/chain-management/test_web3_configured_with_default_from_address.py
+++ tests/chain-management/test_web3_configured_with_default_from_address.py
@@ -1,8 +1,17 @@
-def test_chain_web3_is_preconfigured_with_default_from(project):
-    default_account = '0x0000000000000000000000000000000000001234'
-    project.config['web3.Tester.eth.default_account'] = default_account
+from populus.chain.helpers import (
+    get_chain,
+)
 
-    with project.get_chain('tester') as chain:
+from populus.api.config import (
+    write_user_config,
+)
+
+def test_chain_web3_is_preconfigured_with_default_from(project, user_config, user_config_path):
+    default_account = '0x0000000000000000000000000000000000001234'
+    user_config['web3.Tester.eth.default_account'] = default_account
+    write_user_config(user_config, user_config_path)
+
+    with get_chain('tester', user_config, chain_dir=project.project_root_dir) as chain:
         web3 = chain.web3
 
         assert web3.eth.defaultAccount == default_account

7caa26da46ed09c2fcf3f858dffe74667a089ca1
aviah <aviah@gmail.com>
2017-08-29 06:54:38 -0700

    cleanup main defaults

---
+++ populus/defaults.py
@@ -0,0 +1,29 @@
+#
+# Config
+#
+USER_JSON_CONFIG_FILENAME = '.populus/populus.config.json'
+USER_JSON_CONFIG_DEFAULTS = 'user.config.defaults.json'
+USER_JSON_CONFIG_SCHEMA = 'user.config.schema.json'
+
+PROJECT_JSON_CONFIG_FILENAME = './project.json'
+PROJECT_JSON_CONFIG_DEFAULTS = 'project.config.defaults.json'
+PROJECT_JSON_CONFIG_SCHEMA = 'project.config.schema.json'
+
+#
+# Compile
+#
+DEFAULT_CONTRACTS_DIR = "./contracts/"
+DEFAULT_TESTS_DIR = "./tests/"
+BUILD_ASSET_DIR = "./build"
+COMPILED_CONTRACTS_ASSET_FILENAME = './contracts.json'
+
+#
+# Deploy
+#
+DEPLOY_JSON_CONFIG_FILENAME = "./deployment.json"
+
+
+#
+# Chain
+#
+BASE_BLOCKCHAIN_STORAGE_DIR = "./chains"

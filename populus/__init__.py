d60b644476c721a0c0886d788941f7b6ae89e71b
aviah <aviah@gmail.com>
2017-10-02 08:52:59 -0700

    depr warning python2

--- populus/__init__.py
+++ populus/__init__.py
@@ -1,6 +1,14 @@
 import os
-
 import pkg_resources
+import warnings
+import sys
+
+if sys.version_info.major < 3:
+    warn_msg = ("Python 2 support will end during the first quarter of 2018"
+                "Please upgrade to Python 3"
+                "https://medium.com/@pipermerriam/dropping-python-2-support-d781e7b48160"
+                )
+    warnings.warn(warn_msg, DeprecationWarning)
 
 
 BASE_DIR = os.path.dirname(__file__)

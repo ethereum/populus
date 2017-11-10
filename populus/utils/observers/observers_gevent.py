bf072abbacbfc8e53fa3eca947b74e03e7e5aace
Piper Merriam <pipermerriam@gmail.com>
2017-11-10 09:38:20 -0700

    fix flake8

--- populus/utils/observers/observers_gevent.py
+++ populus/utils/observers/observers_gevent.py
@@ -40,7 +40,7 @@ class GeventObserver(object):
             try:
                 with gevent.Timeout(2):
                     hub.wait(watcher)
-            except:
+            except:  # noqa: E722
                 continue
             if os.path.isdir(path):
                 new = set(os.listdir(path))

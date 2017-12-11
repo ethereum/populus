414319ae64e305a7db0183b20b209944a2959dd4
Noel Maersk <veox@users.noreply.github.com>
2017-12-11 16:01:07 +0200

    tests: add LLLBackend test.

--- tests/compilation/test_lll_backend.py
+++ tests/compilation/test_lll_backend.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 from populus.compilation import (
@@ -7,8 +6,27 @@ from populus.compilation import (
 
 from populus.utils.testing import (
     load_contract_fixture,
-    #viper_installed,
+    update_project_config,
 )
 
-# TODO: stub!
-assert False
+@load_contract_fixture('Greeter.lll')
+@load_contract_fixture('Greeter.lll.abi')
+@update_project_config(
+    (
+        'compilation.backend.class',
+        'populus.compilation.backends.LLLBackend',
+    ),
+)
+def test_compiling_lll_project_contracts(project):
+    source_paths, compiled_contracts = compile_project_contracts(project)
+
+    assert 'contracts/Greeter.lll' in source_paths
+
+    assert 'Greeter' in compiled_contracts
+    contract_data = compiled_contracts['Greeter']
+    assert 'bytecode' in contract_data
+    assert 'bytecode_runtime' in contract_data
+    assert 'abi' in contract_data
+    function_names = [x['name'] for x in contract_data['abi'] if x['type'] != 'constructor']
+    assert 'setGreeting' in function_names
+    assert 'greet' in function_names

c80942691b0a8d3e51a4e0df957f4954ac2f181b
Cryptomental <cryptomental.com@gmail.com>
2018-06-22 16:19:02 +0200

    utils/six: Remove Python2 six version.
    Remove implementation and conditional import in __init__.
    
    Refs: #465

--- populus/utils/six/__init__.py
+++ populus/utils/six/__init__.py
@@ -1,15 +1,5 @@
-import sys
-
-
-if sys.version_info.major == 2:
-    from .six_py2 import (
-        queue,
-        configparser,
-        parse,
-    )
-else:
-    from .six_py3 import (  # noqa: #401
-        queue,
-        configparser,
-        parse,
-    )
+from .six_py3 import (  # noqa: #401
+    queue,
+    configparser,
+    parse,
+)
--- populus/utils/six/six_py2.py
+++
@@ -1,3 +0,0 @@
-import queue as queue  # noqa: #401
-import configparser as configparser # noqa: #401
-import urllib.parse as parse  # noqa: F401

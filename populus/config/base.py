5a01c586be6f802f195e09cef3cfe17e109a8281
Cryptomental <cryptomental.com@gmail.com>
2018-06-22 16:26:06 +0200

    config/base: Revert 2to3 __nonzero__ change.
    2to3 tool incorrectly changed __nonzero__ to __bool__,
    leaving two methods with the same name.
    
    Refs: #465

--- populus/config/base.py
+++ populus/config/base.py
@@ -141,7 +141,7 @@ class Config(object):
     def __bool__(self):
         return bool(self._wrapped)
 
-    def __bool__(self):
+    def __nonzero__(self):
         return self.__bool__()
 
     def __len__(self):

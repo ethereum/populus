a44498420c07e9a5966b38e4a89eee9b8192b6e7
voith <voithjm1@gmail.com>
2018-07-27 22:28:31 +0530

    moved items to next line

--- populus/contracts/backends/filesystem.py
+++ populus/contracts/backends/filesystem.py
@@ -95,8 +95,8 @@ class JSONFileBackend(BaseContractBackend):
                 data = valmap(
                     lambda x: x['timestamp'],
                     chain_deployments[instance_identifier]
-                ).items()
-                yield tuple(map(lambda args: ContractMeta(*args), data))
+                )
+                yield tuple(map(lambda args: ContractMeta(*args), data.items()))
 
     #
     # Private API
